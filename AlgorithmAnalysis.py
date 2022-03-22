e98c4a46b877cb4200035b715994b410bb7bf7a6
FLYGUY4400 <daws.bremner@my.wheaton.edu>
2022-03-22 12:59:00 -0500

    Commit

--- AlgorithmAnalysis.py
+++ AlgorithmAnalysis.py
@@ -6,6 +6,30 @@
 The seven functions used in this book
 
 1. The constant function f(n) = 3
+
+2. The Logarithm Function
+f(n) = log_b(n) for some constant b>1
+
+3. The Linear Function
+
+4. N-Log-N Function
+
+5. Quadratic
+
+6. Cubic Function
+
+7. Polynomials
+
 """
 
-"Hello"
+#Section 3.2 Asymptotic Analysis
+#We want to determine the runtime based on the component that grows the fastest
+
+#Section 3.3 The "Big-Oh" Notation
+""" 
+f(n) \in O(g(n)) 
+This is often used to characterize running times and space bounds 
+We can ignore constant factors and lower-order terms 
+"""
+
+#stopped on page 127
